93dee8a564d2cd73ddbd60a0ef5bdc347f479a9b
Aroosh Rana <ar.rana@gemsol.in>
2022-07-24 11:40:03 +0530

    Updating __init__.py to contain create_app function callable from Procfile

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,49 +1,53 @@
 from os import abort
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app():
 
-projects = [
-    {
-        "name": "Habit tracking app with Python and MongoDB",
-        "thumb": "images/habit-tracker.jpg",
-        "hero": "images/habit-tracker.jpg",
-        "categories": ["python", "flask", "html", "css"],
-        "slug": "habit-tracking",
-        "prod": "https://custom-habit-tracker.herokuapp.com",
-        "code": "https://github.com/arooshrana2/habit-tracker"
-    },
-    {
-        "name": "Micro Blog with Flask and MongoDB",
-        "thumb": "images/micro-blog.jpg",
-        "hero": "images/micro-blog-hero.jpg",
-        "categories": ["python", "flask", "html", "css"],
-        "slug": "micro-blog",
-        "prod": "http://microblog-python-flask.herokuapp.com",
-        "code": "https://github.com/arooshrana2/microblog-python-flask"
-    },
-]
+    app = Flask(__name__)
 
-slug_for_projects = {project['slug']: project for project in projects}
+    projects = [
+        {
+            "name": "Habit tracking app with Python and MongoDB",
+            "thumb": "images/habit-tracker.jpg",
+            "hero": "images/habit-tracker.jpg",
+            "categories": ["python", "flask", "html", "css"],
+            "slug": "habit-tracking",
+            "prod": "https://custom-habit-tracker.herokuapp.com",
+            "code": "https://github.com/arooshrana2/habit-tracker"
+        },
+        {
+            "name": "Micro Blog with Flask and MongoDB",
+            "thumb": "images/micro-blog.jpg",
+            "hero": "images/micro-blog-hero.jpg",
+            "categories": ["python", "flask", "html", "css"],
+            "slug": "micro-blog",
+            "prod": "http://microblog-python-flask.herokuapp.com",
+            "code": "https://github.com/arooshrana2/microblog-python-flask"
+        },
+    ]
 
-@app.route('/')
-def home():
-    return render_template('home.html', projects=projects)
+    slug_for_projects = {project['slug']: project for project in projects}
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    @app.route('/')
+    def home():
+        return render_template('home.html', projects=projects)
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
 
-@app.route('/project/<string:slug>')
-def project_page(slug: str):
-    if slug not in slug_for_projects:
-        abort(404)
+    @app.route('/contact')
+    def contact():
+        return render_template('contact.html')
 
-    return render_template(
-        f'project_{slug}.html',
-        project=slug_for_projects[slug]
-    )
+    @app.route('/project/<string:slug>')
+    def project_page(slug: str):
+        if slug not in slug_for_projects:
+            abort(404)
+
+        return render_template(
+            f'project_{slug}.html',
+            project=slug_for_projects[slug]
+        )
+        
+    return app
